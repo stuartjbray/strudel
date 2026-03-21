a301113f13e1715ae6e61b708fe5c7462aa66865
Stuart Bray <stuartjbray@hotmail.com>
2026-03-21 21:28:34 +0000

    .

--- docs/library.js
+++ docs/library.js
@@ -1,10 +1,5 @@
 
 
-Pattern.prototype.mask_ = function(n) {
-  return this.mask("<-@" + n + " 1>")
-}
-
-
-// var mask2 = (x) => x.mask("<-@1 1>")
-// var mask4 = (x) => x.mask("<-@3 1>")
-// var mask8 = (x) => x.mask("<-@7 1>")
+Pattern.prototype.mask2 = function() {  return this.mask("<-@1 1>") }
+Pattern.prototype.mask4 = function() {  return this.mask("<-@3 1>") }
+Pattern.prototype.mask8 = function() {  return this.mask("<-@7 1>") }
