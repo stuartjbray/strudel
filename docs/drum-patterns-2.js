4f4d8786cbbb8dfc86c05f0b0e886703715125da
Stuart Bray <stuartjbray@hotmail.com>
2026-03-21 21:34:41 +0000

    .

--- docs/drum-patterns-2.js
+++ docs/drum-patterns-2.js
@@ -77,3 +77,126 @@ var footwork = stack(
   sound("hh").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0").gain(".4 .9 .4 .7"),
   sound("rim").struct("0 0 1 1 0 0 0 1 0 0 1 0 0 1 0 0").gain(".6"),
 )
+
+
+// --------------------------------------------------------------- Fills
+
+// JUNGLE
+var jungle_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0"),
+  sound("sd").struct("0 0 1 0 0 0 1 0 0 0 1 0 1 1 1 1").room(".4"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".4 .6 .8 1"),
+)
+var jungle_fill2 = stack(
+  sound("bd").struct("1 1 1 0 1 1 0 1 1 0 1 1 0 1 1 1"),
+  sound("sd").struct("0 0 0 1 0 0 1 0 0 1 0 0 1 1 1 1").room(".6"),
+  sound("oh").struct("1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 1").gain(".5 1"),
+)
+
+// AFROBEATS
+var afrobeats_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("sd").struct("0 0 1 0 0 1 0 0 0 1 0 0 1 1 1 1").room(".3"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".5 .7 .5 1"),
+)
+var afrobeats_fill2 = stack(
+  sound("bd").struct("1 1 0 1 1 0 1 1 0 1 1 0 1 1 1 1"),
+  sound("rim").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1").gain(".6 1"),
+  sound("sd").struct("0 0 0 0 1 0 0 0 0 0 0 0 1 1 1 1").room(".5"),
+)
+
+// UKG
+var ukg_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("sd").struct("0 0 1 0 0 1 0 0 0 1 0 0 1 1 1 1").room(".2"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".4 .7 .4 1"),
+)
+var ukg_fill2 = stack(
+  sound("bd").struct("1 1 1 0 1 1 0 0 1 1 0 1 0 1 1 1"),
+  sound("oh").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1").gain(".5 .9"),
+  sound("sd").struct("0 0 0 0 0 0 1 0 0 0 0 0 1 1 1 1").room(".4"),
+)
+
+// REGGAETON
+var reggaeton_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("sd").struct("0 0 1 0 0 1 0 0 0 1 0 0 1 1 1 1").room(".2"),
+  sound("rim").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1").gain(".6"),
+)
+var reggaeton_fill2 = stack(
+  sound("bd").struct("1 1 0 1 1 0 1 1 1 0 1 1 0 1 1 1"),
+  sound("sd").struct("0 0 0 0 1 0 0 1 0 0 0 0 1 1 1 1").room(".5"),
+  sound("rim").struct("0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1").gain(".5 .8"),
+)
+
+// SAMBA
+var samba_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("rim").struct("0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1").gain(".7"),
+  sound("sd").struct("0 0 0 0 1 0 0 0 0 0 0 0 1 1 1 1").room(".4"),
+)
+var samba_fill2 = stack(
+  sound("bd").struct("1 1 0 1 0 1 0 1 1 0 1 0 1 1 1 1"),
+  sound("rim").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1").gain(".5 .9"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".3 .7"),
+)
+
+// TECHNO
+var techno_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("sd").struct("0 0 0 0 1 0 0 0 0 0 0 0 1 1 1 1").room(".6"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".4 .6 .8 1"),
+)
+var techno_fill2 = stack(
+  sound("bd").struct("1 1 1 1 0 1 1 1 1 1 0 1 1 1 1 1"),
+  sound("rim").struct("0 0 1 0 0 0 1 0 0 0 1 0 1 1 1 1").room(".8").gain(".5"),
+  sound("sd").struct("0 0 0 0 1 0 0 0 0 0 0 0 1 1 1 1").room(".9"),
+)
+
+// BOSSA NOVA
+var bossanova_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0"),
+  sound("rim").struct("1 1 0 1 1 0 1 1").gain(".6 .9"),
+  sound("sd").struct("0 0 1 0 1 0 1 1").room(".4"),
+)
+var bossanova_fill2 = stack(
+  sound("bd").struct("1 1 0 1 0 1 1 1"),
+  sound("rim").struct("1 0 1 1 0 1 1 1").gain(".5 1"),
+  sound("hh").struct("1 1 1 1 1 1 1 1").gain(".3 .6 .3 .9"),
+)
+
+// GRIME
+var grime_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("sd").struct("0 0 0 0 1 0 0 0 0 0 0 0 1 1 1 1").room(".2"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".4 .7 .4 1"),
+)
+var grime_fill2 = stack(
+  sound("bd").struct("1 1 0 1 1 0 0 1 1 0 1 1 0 1 1 1"),
+  sound("sd").struct("0 0 0 0 0 0 1 0 0 0 0 0 1 1 1 1").room(".5"),
+  sound("rim").struct("0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1").gain(".5 .8"),
+)
+
+// SECOND LINE
+var secondline_fill1 = stack(
+  sound("bd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1"),
+  sound("sd").struct("0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1").room(".3"),
+  sound("rim").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1").gain(".6"),
+)
+var secondline_fill2 = stack(
+  sound("bd").struct("1 1 0 1 1 0 1 0 1 1 0 1 0 1 1 1"),
+  sound("sd").struct("1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1").room(".5").gain(".7 1"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".3 .7"),
+)
+
+// FOOTWORK
+var footwork_fill1 = stack(
+  sound("bd").struct("1 1 0 1 1 0 1 0 1 1 0 1 0 1 1 1"),
+  sound("sd").struct("0 0 0 0 1 0 0 0 0 0 0 0 1 1 1 1").room(".2"),
+  sound("hh").struct("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1").gain(".4 .7 .4 1"),
+)
+var footwork_fill2 = stack(
+  sound("bd").struct("1 1 1 0 1 1 1 0 1 1 1 0 1 1 1 1"),
+  sound("sd").struct("0 0 1 0 0 0 1 0 0 0 1 0 1 1 1 1").room(".4"),
+  sound("rim").struct("1 0 0 1 0 0 1 0 1 0 0 1 1 1 1 1").gain(".5 .9"),
+)
