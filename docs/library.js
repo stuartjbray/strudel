c42210aba8a5e4db0e23a378891916f9c5c7b960
Stuart Bray <stuartjbray@hotmail.com>
2026-03-21 20:55:08 +0000

    .

--- docs/library.js
+++ docs/library.js
@@ -1,4 +1,10 @@
-var bassPass = (x)   => stack(
-  n(x).scale("c4:major").sound("supersaw").color("orange").attack(1),
-  n(x).scale("c2:major").sound("supersaw").color("orange").attack(0).decay(1).sustain(0),
-  ).lpf(1000).lpq(20)
+
+
+Pattern.prototype.mask_ = function(n) {
+  return this.mask("<-@" + n + " 1>")
+}
+
+
+// var mask2 = (x) => x.mask("<-@1 1>")
+// var mask4 = (x) => x.mask("<-@3 1>")
+// var mask8 = (x) => x.mask("<-@7 1>")
