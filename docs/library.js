f2053ad1ba2893ddf0ce28421ce96becc746bb8d
Stuart Bray <stuartjbray@hotmail.com>
2026-03-29 12:14:57 +0100

    .

--- docs/library.js
+++ docs/library.js
@@ -1,5 +1,7 @@
-var libraryLoaded = true
+var libraryLoaded = true;
 
-Pattern.prototype.mask2 = function() {  return this.mask("<-@1 1>") }
-Pattern.prototype.mask4 = function() {  return this.mask("<-@3 1>") }
-Pattern.prototype.mask8 = function() {  return this.mask("<-@7 1>") }
+const _Pattern = note("c").constructor;
+
+_Pattern.prototype.mask2 = function() { return this.mask("<-@1 1>") }
+_Pattern.prototype.mask4 = function() { return this.mask("<-@3 1>") }
+_Pattern.prototype.mask8 = function() { return this.mask("<-@7 1>") }
