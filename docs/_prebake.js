e860b04d9538508cbb215a53b071e97a89bcfa0c
Stuart Bray <stuartjbray@hotmail.com>
2026-05-24 14:34:13 +0100

    .

--- docs/_prebake.js
+++ docs/_prebake.js
@@ -11,8 +11,10 @@ registerFunc('tinnyOut', (pat,n) => pat.hpf(cosine.range(1500,0).slow(n*2)))
 // commonly used mask patterns.
 window.m2 = "<- 1>"; 
 window.m_2 = "<1 ->"; 
-window.m4 = "<- - - 1>"; 
-window.m_4 = "<1 1 1 ->"  
+window.m4 = "<-@3 1>"; 
+window.m_4 = "<1@3 ->"  
+window.m8 = "<-@7 1>"; 
+window.m_8 = "<1@7 ->"  
 
 // colours
 window.cols = ["#ff0000","#ff8700","#ffd300","#deff0a","#a1ff0a","#0aff99","#0aefff","#147df5","#580aff","#be0aff"]
